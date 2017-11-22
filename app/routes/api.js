256b9d6af67920e87bc24c49d2b5841b2b4d4d6f
Jesus Vazquez <sikios182@hotmail.com>
2017-11-22 16:38:28 -0600

    Add All Files

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,12 +3,17 @@
 var router = require('express').Router();
 
 var config = require('../config'),
-	AuthController = require('../controllers/authController');
+	AuthController = require('../controllers/authController'),
+	allowOnly = require('../services/routeHelper').allowOnly,
+	UserController = require('../controllers/userController'),
+	AdminController = require('../controllers/adminController');
 
 var APIRoutes = function(passport) {
 	//TODO: create API routes
 	router.post('/signup', AuthController.signUp);
 	router.post('/authenticate', AuthController.authenticateUser);
+	router.get('/profile', passport.authenticate('jwt', { session: false }), allowOnly(config.accessLevels.user, UserController.index));
+	router.get('/admin', passport.authenticate('jwt', { session:false }), allowOnly(config.accessLevels.admin, AdminController.index));
 
 	return router;
 };
